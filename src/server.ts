a9c3aa33b2171bfca06891eab83adff6a7aa2c65
David Nafie <166964151+devo246@users.noreply.github.com>
2026-08-28 10:45:19 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173/"}));
+app.use(cors());
 app.use(cookieParser());
 
 dotenv.config();
